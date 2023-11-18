598c87a2e56c90779bf571a2491de6d9fba183be
MarioGit19 <145853739+MarioGit19@users.noreply.github.com>
2023-11-19 00:11:30 +0100

    Initial commit

---
+++ src/components/Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info(props) {
+  return (
+    <div>
+      {" "}
+      <p className="info">{props.tel}</p>
+      <p className="info">{props.email}</p>
+    </div>
+  );
+}
+
+export default Info;
